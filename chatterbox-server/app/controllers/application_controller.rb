dec7971e716ded4cbfe821cbc004e10f7770a0d2
jacob <jacobmjones17@gmail.com>
2022-09-25 18:15:25 -0400

    commit tree 3a285476d7803257171e69161c89b380fa1e7d50

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -2,5 +2,34 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add routes
-  
+
+  post "/messages" do
+    messages = Message.create(
+      body: params[:body],
+      username: params[:username],
+      created_at: params[:created_at],
+      updated_at: params[:updated_at]
+    )
+    messages.to_json
+  end
+
+  patch "/messages/:id" do
+    messages = Message.find(params[:id])
+    messages.update(
+      body: params[:body],
+      updated_at: params[:updated_at]
+    )
+    messages.to_json
+  end
+
+  delete "/messages/:id" do
+    messages = Message.find(params[:id])
+    messages.destroy
+    messages.to_json
+  end
+
+  get "/messages" do 
+    messages = Message.all.order(:created_at)
+    messages.to_json
+  end
 end
